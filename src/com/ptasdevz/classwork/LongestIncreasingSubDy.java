51db474d61e6788242c62e34d7c21eff0529427a
peepaz <peepaz@gmail.com>
2017-10-14 20:28:25 -0400

    update

---
+++ src/com/ptasdevz/classwork/LongestIncreasingSubDy.java
@@ -0,0 +1,32 @@
+package com.ptasdevz.classwork;
+
+/**
+ * Created by jason on 14/10/2017.
+ */
+public class LongestIncreasingSubDy {
+
+    static int[] A = {35,31,22,30,27,29,21,32,24,28};
+    static  int [] L = new int[A.length];
+    static  int [] E = new int[A.length];
+
+    public static void main (String args[]){
+        
+        //initializde arrays
+        for (int i = 0; i < L.length; i++) {
+            L[i] = 1;
+            E[i] =0;
+        }
+
+    }
+
+    public static void getLis(){
+
+        for (int i = 0; i <L.length ; i++) {
+
+            for (int j = 0; j < i; j++) {
+                
+            }
+            
+        }
+    }
+}
