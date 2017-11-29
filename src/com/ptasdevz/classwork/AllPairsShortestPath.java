6f34dd9807408ba3ed1efce8354996319663698e
peepaz <peepaz@gmail.com>
2017-11-28 21:10:12 -0400

    Update AllPairsShortestPath.java

--- src/com/ptasdevz/classwork/AllPairsShortestPath.java
+++ src/com/ptasdevz/classwork/AllPairsShortestPath.java
@@ -13,53 +13,80 @@ public class AllPairsShortestPath {
         D = new int[5][5];
         path = new int [5][5];
         D[1][1] = 0;
-        D[1][2] = 9999;
+        D[1][2] = 999;
         D[1][3] = 4;
-        D[1][4] = 9999;
-        D[2][1] = 9999;
+        D[1][4] = 999;
+
+        D[2][1] = 999;
         D[2][2] = 0;
-        D[2][3] = 9999;
+        D[2][3] = 999;
         D[2][4] = 7;
-        D[3][1] = 18;
+
+        D[3][1] = 16;
         D[3][2] = 5;
         D[3][3] = 0;
         D[3][4] = 999;
+
         D[4][1] = 3;
         D[4][2] = 999;
-        D[4][3] = 9;
+        D[4][3] = 8;
         D[4][4] = 0;
+        init();
         getAllPairsShortestPath();
+        System.out.printf("\n");
+        printVals(D);
+        printAllpaths(3);
+
 
 
     }
 
     public static void getAllPairsShortestPath(){
 
+
+
         for (int k = 1; k < D.length; k++) {
 
             for (int i = 1; i < D.length; i++) {
-                for (int j = 1; j < D[i].length; j++) {
+                for (int j = 1; j < D.length; j++) {
                     int intermediaryCost = (D[i][k] + D[k][j]);
                     if (intermediaryCost < D[i][j]){
                         D[i][j] = intermediaryCost;
-                        path[i][j] = k; // store parent node (intermediary node)
+                        path[i][j] = path[k][j]; // store parent node (intermediary node)
                     }
 //                    D[i][j] = min(D[i][j],intermediaryCost);
 
                 }
             }
-            System.out.printf("\n");
-            printVals(D);
-            System.out.printf("\n");
-            printVals(path);
+//            System.out.printf("\n");
+//            printVals(D);
 
         }
+//        System.out.printf("\n");
+//        printVals(path);
     }
 
     private static int min(int n, int m) {
 
         if (n<m)return n;
         return m;
+    }
+
+    private static void init(){
+        for (int i = 0; i < D.length; i++) {
+            for (int j = 0; j < D.length; j++) {
+                if (i==j){
+                    path[i][j] = 0;
+                }
+                else if (D[i][j] != 999){
+                    path[i][j] = i;
+                }
+                else {
+                    path[i][j] = 0;
+                }
+            }
+
+        }
     }
 
     private static void printVals(int[][] a){
@@ -70,4 +97,28 @@ public class AllPairsShortestPath {
             System.out.println();
         }
     }
+
+    private static void printAllpaths(int v){
+
+        for (int i = 1; i < D.length; i++) {
+            if (i != v){
+                System.out.printf("Shortest path to %d:",i);
+                printAllpathHelper(v,i);
+                System.out.printf("\n");
+            }
+        }
+    }
+
+    private static void printAllpathHelper(int u, int v){
+
+        if (u == v) System.out.printf("%d",v);
+        else {
+            printAllpathHelper(u, path[u][v]);
+            System.out.printf(" -> %d",v);
+        }
+
+    }
+
+
+
 }
